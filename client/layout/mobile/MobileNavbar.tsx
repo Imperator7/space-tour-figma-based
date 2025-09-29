d9108225867aaafb504bcbda8aa27357808bd484
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-09-29 15:15:49 +0700

    make mobile navbar close when click outside of it

--- client/layout/mobile/MobileNavbar.tsx
+++ client/layout/mobile/MobileNavbar.tsx
@@ -42,22 +42,30 @@ const MobileNavbar = ({
   return (
     <div
       className={[
-        'fixed z-50 bg-[#0B0D17]/15 backdrop-blur-[40px] right-0 h-dvh',
-        'pl-400 flex flex-col gap-600',
-        'transition-all duration-500',
+        'flex h-dvh inset-0',
+        'z-50 right-0',
+        'fixed transition-all duration-500',
         toggleNavbar ? '' : 'translate-x-full',
       ].join(' ')}
     >
-      <CloseButton handleClose={handleCloseNavbar} />
-      <div className="flex flex-col gap-400">
-        {links.map((link, i) => (
-          <NavItem
-            key={link.name}
-            selected={pathname === link.href}
-            linkData={link}
-            index={i}
-          />
-        ))}
+      <div className="w-full" onClick={handleCloseNavbar}></div>
+      <div
+        className={[
+          'bg-[#0B0D17]/15 backdrop-blur-[40px]',
+          'pl-400 flex flex-col gap-600',
+        ].join(' ')}
+      >
+        <CloseButton handleClose={handleCloseNavbar} />
+        <div className="flex flex-col gap-400">
+          {links.map((link, i) => (
+            <NavItem
+              key={link.name}
+              selected={pathname === link.href}
+              linkData={link}
+              index={i}
+            />
+          ))}
+        </div>
       </div>
     </div>
   )
