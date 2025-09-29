da95a13928139d06ed6aec620b3b8aaf78a26645
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-09-29 10:47:25 +0700

    make TabMenu component

--- client/components/TabMenu.tsx
+++ client/components/TabMenu.tsx
@@ -1,4 +1,23 @@
-const TabMenu = () => {
-  return <div>TabMenu</div>
+import TabItem from './TabItem'
+import { Planet, PLANETS } from '@/lib/planets'
+
+type TabMenuProps = {
+  selectedTab: Planet
+  handleSelectTab: (tabName: Planet) => void
+}
+
+const TabMenu = ({ selectedTab, handleSelectTab }: TabMenuProps) => {
+  return (
+    <div className="flex gap-400">
+      {PLANETS.map((planet) => (
+        <TabItem
+          key={planet}
+          selected={selectedTab === planet}
+          tabName={planet}
+          handleClick={() => handleSelectTab(planet)}
+        />
+      ))}
+    </div>
+  )
 }
 export default TabMenu
