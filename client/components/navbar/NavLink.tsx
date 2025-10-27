2740646e7ca40a18c50a7205bdf157aa37b2f827
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-10-27 16:45:21 +0700

    refactor(navbar): split navbar layout into types

--- client/components/navbar/NavLink.tsx
+++ client/components/navbar/NavLink.tsx
@@ -1,39 +1,61 @@
-import { LinkObject } from '@/layout/MainLayout'
+import { LinkObject } from '@/lib/constants/links'
 import Link from 'next/link'
 
 type NavLinkProps = {
   selected: boolean
   linkData: LinkObject
   index: number
+  className?: string
 }
 
-const NavLink = ({ selected, linkData, index }: NavLinkProps) => {
-  return (
-    <Link
-      href={linkData.href}
+const MobileLayout = ({
+  selected,
+  linkData,
+  index,
+  className,
+}: NavLinkProps) => (
+  <div
+    className={[
+      'flex justify-between group h-[19px] cursor-pointer',
+      className || '',
+    ].join(' ')}
+  >
+    <div className="flex gap-[12px] h-[19px]">
+      <p className="text-8 font-bold leading-none tabular-nums">
+        {'0' + index}
+      </p>
+      <p className="text-8 leading-none">{linkData.name.toUpperCase()}</p>
+    </div>
+    <div
       className={[
-        'flex  justify-between group h-[19px] cursor-pointer',
-        'md:flex-col md:h-full md:items-center md:relative md:justify-center',
+        'w-[3px]  bg-white opacity-0 transition-opacity duration-300 delay-50 ease-in',
+        selected ? 'opacity-100' : 'group-hover:opacity-50',
+      ].join(' ')}
+    />
+  </div>
+)
+
+const TabletLayout = ({
+  selected,
+  linkData,
+  index,
+  className,
+}: NavLinkProps) => {
+  return (
+    <div
+      className={[
+        className || '',
+        'group h-full cursor-pointer',
+        'h-full items-center relative justify-center',
       ].join(' ')}
     >
-      <div className="flex gap-[12px]">
+      <div className="flex gap-[12px] h-[19px] leading-[19px]">
         <p className="text-8 font-bold leading-none tabular-nums">
           {'0' + index}
         </p>
         <p className="text-8 leading-none">{linkData.name.toUpperCase()}</p>
       </div>
-      <div
-        className={[
-          'md:hidden',
-          'w-[3px]  bg-white opacity-0 transition-opacity duration-300 delay-50 ease-in',
-          selected ? 'opacity-100' : 'group-hover:opacity-50',
-        ].join(' ')}
-      />
-      <div
-        className={[
-          'hidden h-full w-full md:flex md:absolute md:items-end',
-        ].join(' ')}
-      >
+      <div className={['flex h-full w-full absolute items-end'].join(' ')}>
         <div
           className={[
             'bg-white w-full h-[3px] opacity-0 transition-opacity duration-300 delay-50 ease-in',
@@ -41,6 +63,27 @@ const NavLink = ({ selected, linkData, index }: NavLinkProps) => {
           ].join(' ')}
         ></div>
       </div>
+    </div>
+  )
+}
+
+const DesktopNavbar = () => {}
+
+const NavLink = ({ selected, linkData, index }: NavLinkProps) => {
+  return (
+    <Link href={linkData.href} className="h-full">
+      <MobileLayout
+        selected={selected}
+        linkData={linkData}
+        index={index}
+        className="md:hidden"
+      />
+      <TabletLayout
+        selected={selected}
+        linkData={linkData}
+        index={index}
+        className="hidden md:flex"
+      />
     </Link>
   )
 }
