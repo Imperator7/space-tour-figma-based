2edfbd74d281e65d50672d6f41d3a22db9fbb068
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-10-28 16:01:46 +0700

    feat(responsive): make tablet version for destination page

--- client/app/destination/page.tsx
+++ client/app/destination/page.tsx
@@ -13,17 +13,21 @@ const Destination = () => {
   return (
     <div
       className={[
+        'h-full',
         'preserve-navbar-space min-h-dvh',
-        "bg-cover bg-[url('/assets/destination/background-destination-mobile.jpg')]",
+        'bg-cover bg-no-repeat bg-center',
+        "bg-[url('/assets/destination/background-destination-mobile.jpg')]",
+        "md:bg-[url('/assets/destination/background-destination-tablet.jpg')]",
+        "lg:bg-[url('/assets/destination/background-destination-desktop.jpg')]",
       ].join(' ')}
     >
       <div
         className={[
           'flex flex-col items-center',
-          'h-full p-300 gap-300',
-        ].join()}
+          'md:h-full p-300 md:p-500 gap-300',
+        ].join(' ')}
       >
-        <h3 className="flex h-[19px] gap-300 items-center justify-center">
+        <h3 className="flex h-[19px] md:h-[24px] gap-300 items-center justify-center md:justify-start w-full">
           <span className="text-8 font-bold tabular-nums leading-none opacity-25">
             01
           </span>
@@ -32,56 +36,61 @@ const Destination = () => {
           </span>
         </h3>
 
-        <div
-          className={[
-            'h-[202px] flex justify-center items-center',
-            'transition-all duration-2000',
-          ].join(' ')}
-        >
-          <picture>
-            <source
-              srcSet={`/assets/destination/image-${selectedTab.toLowerCase()}.webp`}
-              type="image/webp"
-            />
-            <Image
-              src={`/assets/destination/image-${selectedTab.toLowerCase()}.png`}
-              width={150}
-              height={150}
-              alt={selectedTab}
-            />
-          </picture>
-        </div>
-        <div className="flex flex-col gap-300 items-center h-full">
-          <TabMenu
-            selectedTab={selectedTab}
-            handleSelectTab={handleSelectTab}
-          />
-          <div className="flex flex-col gap-200 items-center">
-            <h2 className="text-2 text-[56px]">
-              {PLANET_ASSETS[selectedTab].name.toUpperCase()}
-            </h2>
-            <p className="text-center text-secondary-blue h-[136px]">
-              {PLANET_ASSETS[selectedTab].description}
-            </p>
+        <div className="flex flex-col gap-400">
+          <div
+            className={[
+              'h-[202px] md:h-[384px] flex justify-center items-center',
+              'transition-all duration-2000',
+            ].join(' ')}
+          >
+            <picture>
+              <source
+                srcSet={`/assets/destination/image-${selectedTab.toLowerCase()}.webp`}
+                type="image/webp"
+              />
+              <Image
+                src={`/assets/destination/image-${selectedTab.toLowerCase()}.png`}
+                width={300}
+                height={300}
+                className="w-[150px] h-[150px] md:w-[300px] md:h-[300px] lg:w-[480px] lg:h-[480px]"
+                alt={selectedTab}
+              />
+            </picture>
           </div>
-        </div>
-        <div className="h-[1px] w-full bg-[#979797]/25"></div>
-        <div className="flex flex-col items-center gap-300">
-          <div className="flex flex-col items-center gap-150">
-            <p className="text-7 leading-none text-secondary-blue h-[17px] align-text-center">
-              AVG. DISTANCE
-            </p>
-            <p className="text-6 leading-none h-[32px]">
-              {PLANET_ASSETS[selectedTab].distance.toUpperCase()}
-            </p>
-          </div>
-          <div className="flex flex-col items-center gap-150">
-            <p className="text-7 leading-none text-secondary-blue h-[17px] align-text-center">
-              EST. TRAVEL TIME
-            </p>
-            <p className="text-6 leading-none h-[32px]">
-              {PLANET_ASSETS[selectedTab].travel.toUpperCase()}
-            </p>
+          <div className="flex flex-col gap-300">
+            <div className="flex flex-col gap-300  items-center h-full max-w-[514px]">
+              <TabMenu
+                selectedTab={selectedTab}
+                handleSelectTab={handleSelectTab}
+              />
+              <div className="flex flex-col gap-200 items-center">
+                <h2 className="text-2 text-[56px] md:text-[80px]">
+                  {PLANET_ASSETS[selectedTab].name.toUpperCase()}
+                </h2>
+                <p className="text-center text-secondary-blue text-[15px] md:text-[16px]">
+                  {PLANET_ASSETS[selectedTab].description}
+                </p>
+              </div>
+            </div>
+            <div className="h-[1px] w-full bg-[#979797]/25"></div>
+            <div className="flex flex-col md:flex-row items-center justify-center gap-300">
+              <div className="flex flex-col  items-center gap-150 w-full">
+                <p className="text-7 leading-none text-secondary-blue h-[17px] align-text-center">
+                  AVG. DISTANCE
+                </p>
+                <p className="text-6 leading-none h-[32px]">
+                  {PLANET_ASSETS[selectedTab].distance.toUpperCase()}
+                </p>
+              </div>
+              <div className="flex flex-col items-center gap-150 w-full">
+                <p className="text-7 leading-none text-secondary-blue h-[17px] align-text-center">
+                  EST. TRAVEL TIME
+                </p>
+                <p className="text-6 leading-none h-[32px]">
+                  {PLANET_ASSETS[selectedTab].travel.toUpperCase()}
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
