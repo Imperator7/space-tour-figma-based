6236cb3848a8acd3fb64be7b578dd20168554c1f
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-10-29 17:51:17 +0700

    feat(responsive): make the crew page to be fully responsive for all platforms

--- client/app/crew/page.tsx
+++ client/app/crew/page.tsx
@@ -6,16 +6,21 @@ const Crew = () => {
   return (
     <main
       className={[
-        "min-h-dvh bg-cover bg-no-repeat bg-[url('/assets/crew/background-crew-mobile.jpg')]",
+        'h-dvh bg-cover bg-no-repeat',
         'preserve-navbar-space',
+        "bg-[url('/assets/crew/background-crew-mobile.jpg')]",
+        "md:bg-[url('/assets/crew/background-crew-tablet.jpg')]",
+        "lg:bg-[url('/assets/crew/background-crew-desktop.jpg')]",
+        'overflow-hidden',
+        'lg:flex justify-center',
       ].join(' ')}
     >
-      <div className="flex flex-col p-300 gap-300 h-full">
-        <h3 className="flex h-[19px] gap-300 items-center justify-center">
-          <span className="text-8 font-bold tabular-nums leading-none opacity-25">
+      <div className="flex flex-col p-300 md:p-500 gap-300 lg:p-0 lg:py-600 h-full w-full lg:max-w-[1110px]">
+        <h3 className="flex h-[19px] md:h-[24px] lg:h-[34px] gap-300 items-center justify-center md:justify-start">
+          <span className="text-8 md:text-[20px] lg:text-[28px] font-bold tabular-nums leading-none opacity-25">
             02
           </span>
-          <span className="text-5 text-[16px] leading-none">
+          <span className="text-5 text-[16px] md:text-[20px] lg:text-[28px] leading-none">
             MEET YOUR CREW
           </span>
         </h3>{' '}
